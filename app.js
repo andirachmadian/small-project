98aca81bad332be2672cb96b3df1db2c1c0c4834
Andi Rachmadian <andikharisma24@gmail.com>
2020-03-02 18:49:22 +0700

    edit css and html

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ $('#myTab li:last-child a').tab('show') // Select last tab
 $('#myTab li:nth-child(3) a').tab('show') // Select third tab
 
 // Get the modal
-let modal = document.getElementById("myModal");
+var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-let img = document.getElementById("myImg");
-let modalImg = document.getElementById("img01");
-let captionText = document.getElementById("caption");
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
 img.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
@@ -22,7 +22,7 @@ img.onclick = function(){
 }
 
 // Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() { 
